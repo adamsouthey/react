08ceaadeec5d8a0a6d485ca8feb79ccd3e4e7a57
adamsouthey <adamjsouthey@gmail.com>
2020-03-10 13:34:55 +0000

    []-inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    const [loading, setLoading] = useState(true)
+    const [giphyData, setGiphyData] = useState([])
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        try {
+            await axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=Tr8iKNxQPg6dc2XZ5R4Sa5qNVSfP2L1P`)
+                .then(
+                    response => {
+                        setLoading(false)
+                        setGiphyData(response.data.data)
+                    }
+                )
+        } catch (err) {
+            console.log('Error fetching and parsing data', err.message);
+        }
+    }
+
+    return (
+        <>
+            <h1>GIFS</h1>
+            <div className="container" >
+                {
+                    loading && !giphyData ? <p>Loading GIFs...</p> : giphyData.map((item) => {
+                        return (
+                            <>
+                                <img className="item" key={item.id} src={item.images.downsized_large.url} alt="" />
+                            </>
+                        )
+                    })
+                }
+            </div>
+        </>
+
+    )
+
 }
-
-export default App;
