caa74b56b73c5ec9e7dcf20e272a1397e648d0dc
adamsouthey <adamjsouthey@gmail.com>
2020-03-12 09:59:37 +0000

    changed header color to red

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export const App = () => {
 
     return (
         <>
-            <h1 style={{ color: 'blue', fontSize: '5rem' }}> GIFSs</h1>
+            <h1 style={{ color: 'red', fontSize: '5rem' }}> GIFSs</h1>
             <div className="container" >
                 {
                     loading && !giphyData ? <p>Loading GIFs...</p> : giphyData.map((item) => {
